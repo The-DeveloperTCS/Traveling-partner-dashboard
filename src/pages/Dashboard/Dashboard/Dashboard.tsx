16866f8e2308e4d869d480bee28ebae582ceac5a
Murad khan <murad.jaddoon@gmail.com>
2023-03-16 18:25:12 +0500

    call chart component at dashboard

--- src/pages/Dashboard/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard/Dashboard.tsx
@@ -1,7 +1,43 @@
+import { Grid } from '@mui/material'
 import React from 'react'
+import Card from '../../../components/Card/Card'
+import BarChart from '../../../components/Chart/BarChart'
+import LineChart from '../../../components/Chart/LineChart'
+
+const data = [
+    {
+        value: 'low',
+        month: 1,
+    },
+    {
+        value: 'medium',
+        month: 2,
+    },
+    {
+        value: 'high',
+        month: 3,
+    },
+    {
+        value: 'medium',
+        month: 4,
+    },
+]
 
 const Dashboard = (): ReactNode => {
-    return <div>Dashboard</div>
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+                <Card>
+                    <LineChart height={100} />
+                </Card>
+            </Grid>
+            <Grid item xs={12} md={6}>
+                <Card>
+                    <BarChart data={data} />
+                </Card>
+            </Grid>
+        </Grid>
+    )
 }
 
 export default Dashboard
