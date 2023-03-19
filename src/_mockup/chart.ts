86a11da4713ca0609d9d3877851881356a3e3e96
Murad khan <murad.jaddoon@gmail.com>
2023-03-19 23:00:25 +0500

    added chart mockup

---
+++ src/_mockup/chart.ts
@@ -0,0 +1,30 @@
+export const data = [
+    {
+        value: 'low',
+        sale: 1000,
+    },
+    {
+        value: 'low',
+        sale: 500,
+    },
+    {
+        value: 'medium',
+        sale: 1500,
+    },
+    {
+        value: 'high',
+        sale: 4000,
+    },
+    {
+        value: 'low',
+        sale: 800,
+    },
+    {
+        value: 'medium',
+        sale: 1400,
+    },
+    {
+        value: 'high',
+        sale: 1800,
+    },
+]
