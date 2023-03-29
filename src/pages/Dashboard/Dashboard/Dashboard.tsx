6bb815f8a1d48ea912c7af64f425e029fb318c6e
Murad khan <murad.jaddoon@gmail.com>
2023-03-29 21:29:10 +0500

    protected routes by role wise

--- src/pages/Dashboard/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard/Dashboard.tsx
@@ -13,79 +13,82 @@ import { usersMockup } from '../../../_mockup/users'
 import { columns } from '../../../columns/orders'
 import SubHeading from '../../../components/SubHeading/SubHeading'
 import { data } from '../../../_mockup/chart'
+import Protection from '../../../roles/protection/Protection'
 
 const Dashboard = (): ReactNode => {
     const [loading, setLoading] = useState<boolean>(false)
 
     return (
-        <Grid
-            container
-            spacing={2}
-            alignItems="stretch"
-            justifyContent="stretch"
-        >
-            <Grid item xs={12} sm={6} md={3}>
-                <Widget
-                    title="Daily Views"
-                    value={25000}
-                    Icon={VisibilityOutlinedIcon}
-                />
+        <Protection protection={['admin']}>
+            <Grid
+                container
+                spacing={2}
+                alignItems="stretch"
+                justifyContent="stretch"
+            >
+                <Grid item xs={12} sm={6} md={3}>
+                    <Widget
+                        title="Daily Views"
+                        value={25000}
+                        Icon={VisibilityOutlinedIcon}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                    <Widget
+                        title="Sales"
+                        value={0}
+                        Icon={ShoppingCartOutlinedIcon}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                    <Widget
+                        title="Comments"
+                        value={25000}
+                        Icon={CommentOutlinedIcon}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                    <Widget
+                        title="Earnings"
+                        value={25100}
+                        Icon={PaymentsOutlined}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={5} md={4}>
+                    <Card sx={{ height: '100%' }}>
+                        <SubHeading>Stats</SubHeading>
+                        <PieChart data={data} />
+                    </Card>
+                </Grid>
+                <Grid item xs={12} sm={7} md={8}>
+                    <Card sx={{ height: '100%' }}>
+                        <SubHeading>Sale</SubHeading>
+                        <BarChart data={data} />
+                    </Card>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Card>
+                        <SubHeading>Users</SubHeading>
+                        {useMemo(
+                            () => (
+                                <DynamicTable
+                                    isLoading={loading}
+                                    columns={columns()}
+                                    data={usersMockup}
+                                    total={10}
+                                />
+                            ),
+                            [data, loading]
+                        )}
+                    </Card>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Card>
+                        <SubHeading>Record</SubHeading>
+                    </Card>
+                </Grid>
             </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-                <Widget
-                    title="Sales"
-                    value={0}
-                    Icon={ShoppingCartOutlinedIcon}
-                />
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-                <Widget
-                    title="Comments"
-                    value={25000}
-                    Icon={CommentOutlinedIcon}
-                />
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-                <Widget
-                    title="Earnings"
-                    value={25100}
-                    Icon={PaymentsOutlined}
-                />
-            </Grid>
-            <Grid item xs={12} sm={5} md={4}>
-                <Card sx={{ height: '100%' }}>
-                    <SubHeading>Stats</SubHeading>
-                    <PieChart data={data} />
-                </Card>
-            </Grid>
-            <Grid item xs={12} sm={7} md={8}>
-                <Card sx={{ height: '100%' }}>
-                    <SubHeading>Sale</SubHeading>
-                    <BarChart data={data} />
-                </Card>
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <Card>
-                    <SubHeading>Users</SubHeading>
-                    {useMemo(
-                        () => (
-                            <DynamicTable
-                                isLoading={loading}
-                                columns={columns()}
-                                data={usersMockup}
-                                total={10}
-                            />
-                        ),
-                        [data, loading]
-                    )}
-                </Card>
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <Card>
-                    <SubHeading>Record</SubHeading>
-                </Card>
-            </Grid>
-        </Grid>
+        </Protection>
     )
 }
 
--- src/pages/Dashboard/Profile/Profile.tsx
+++ src/pages/Dashboard/Profile/Profile.tsx
@@ -9,81 +9,84 @@ import SubHeading from '../../../components/SubHeading/SubHeading'
 import BarChart from '../../../components/Chart/BarChart'
 import { data } from '../../../_mockup/chart'
 import LineChart from '../../../components/Chart/LineChart'
+import Protection from '../../../roles/protection/Protection'
 
 const Profile = (): ReactNode => {
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={12} md={4}>
-                <Card sx={{ height: '100%' }}>
-                    <SubHeading>States</SubHeading>
-                    <BarChart data={data} />
-                    <Box mt={2} />
-                    <SubHeading>History</SubHeading>
-                    <LineChart height={200} />
-                </Card>
-            </Grid>
-            <Grid item xs={12} md={8}>
-                <Card sx={{ p: 4, height: '100%' }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <Stack
-                                direction="row"
-                                gap={2}
-                                justifyContent="flex-start"
-                                alignItems="flex-end"
-                            >
-                                <UserAvatar
-                                    url={userImg}
-                                    width={100}
-                                    height={100}
+        <Protection protection={['admin']}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={4}>
+                    <Card sx={{ height: '100%' }}>
+                        <SubHeading>States</SubHeading>
+                        <BarChart data={data} />
+                        <Box mt={2} />
+                        <SubHeading>History</SubHeading>
+                        <LineChart height={200} />
+                    </Card>
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Card sx={{ p: 4, height: '100%' }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Stack
+                                    direction="row"
+                                    gap={2}
+                                    justifyContent="flex-start"
+                                    alignItems="flex-end"
+                                >
+                                    <UserAvatar
+                                        url={userImg}
+                                        width={100}
+                                        height={100}
+                                    />
+                                    <Button>Change Picture</Button>
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextInput
+                                    label="First Name"
+                                    placeholder="john"
+                                    name="firstName"
                                 />
-                                <Button>Change Picture</Button>
-                            </Stack>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextInput
+                                    label="Last Name"
+                                    placeholder="Doe"
+                                    name="lastName"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextInput
+                                    label="Email"
+                                    type="email"
+                                    placeholder="jhon@example.com"
+                                    name="email"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextInput
+                                    label="Phone Number"
+                                    type="number"
+                                    placeholder="12345678901"
+                                    name="phoneNumber"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Stack
+                                    direction="row"
+                                    justifyContent="flex-end"
+                                    gap={2}
+                                >
+                                    {/* <Button variant="outlined">Change Password</Button> */}
+                                    <Button variant="gradient">Update</Button>
+                                </Stack>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextInput
-                                label="First Name"
-                                placeholder="john"
-                                name="firstName"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextInput
-                                label="Last Name"
-                                placeholder="Doe"
-                                name="lastName"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextInput
-                                label="Email"
-                                type="email"
-                                placeholder="jhon@example.com"
-                                name="email"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextInput
-                                label="Phone Number"
-                                type="number"
-                                placeholder="12345678901"
-                                name="phoneNumber"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Stack
-                                direction="row"
-                                justifyContent="flex-end"
-                                gap={2}
-                            >
-                                {/* <Button variant="outlined">Change Password</Button> */}
-                                <Button variant="gradient">Update</Button>
-                            </Stack>
-                        </Grid>
-                    </Grid>
-                </Card>
+                    </Card>
+                </Grid>
             </Grid>
-        </Grid>
+        </Protection>
     )
 }
 
